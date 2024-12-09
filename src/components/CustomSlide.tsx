9e016a2bc225a3c4f78567961e6b469d3fc27d1d
Nutapong.tan <nutapong.tan@gmail.com>
2024-12-09 21:06:02 +0700

    [Update] share to facebook and lock move slice

--- src/components/CustomSlide.tsx
+++ src/components/CustomSlide.tsx
@@ -1,18 +1,23 @@
 import { FC } from "react";
-import { Button, Flex, Typography } from "antd";
+import { Button, Flex, Form, Input, Typography } from "antd";
 import { ISlide } from "../models/slide";
+import TextArea from "antd/es/input/TextArea";
 
 interface SlideProps {
     slide: ISlide;
     active: number;
     total: number;
+    formData: Record<number, any>;
     goto: (slideIndex: number) => void;
-    onPrev: () => void;
-    onNext: () => void;
+    shareForm: () => void;
+    updateForm: (slideIndex: number, data: any) => void;
 }
 
 const { Title, Paragraph } = Typography;
-const CustomSlide: FC<SlideProps> = ({ slide, active, total, onPrev, onNext }) => {
+const CustomSlide: FC<SlideProps> = ({ slide, active, total, formData, goto, updateForm, shareForm }) => {
+    const handleInputChange = (key: string, value: string) => {
+        updateForm(active, { ...formData[active], [key]: value });
+    };
 
     return (
         <div className={`slide-container content-${slide.key}`}>
@@ -22,9 +27,31 @@ const CustomSlide: FC<SlideProps> = ({ slide, active, total, onPrev, onNext }) =
                     {slide.title}
                 </Title>
                 <Paragraph className="slide-description">{slide.description}</Paragraph>
-                <Flex justify="space-between" align="center">
-                    {active !== 0 && (<Button type="primary" onClick={onPrev}>Back</Button>)}
-                    {active + 1 !== total && (<Button type="primary" onClick={onNext}>Next</Button>)}
+                <Form layout="vertical">
+                    <Form.Item label="Your Channel">
+                        <Input
+                            value={formData[active]?.channel || ""}
+                            onChange={(e) => handleInputChange("channel", e.target.value)}
+                        />
+                    </Form.Item>
+                    <Form.Item label="Your Comment">
+                        <TextArea
+                            rows={4}
+                            value={formData[active]?.comment || ""}
+                            onChange={(e) => handleInputChange("comment", e.target.value)}
+                        />
+                    </Form.Item>
+                </Form>
+                <Flex vertical gap={"8px"}>
+                    <Flex justify="center" align="center">
+                        <Button className="facebook-btn" onClick={shareForm}>
+                            Share on Facebook
+                        </Button>
+                    </Flex>
+                    <Flex justify="space-between" align="center">
+                        <Button type="primary" onClick={() => goto(active - 1)} disabled={active === 0}>Back</Button>
+                        <Button type="primary" onClick={() => goto(active + 1)} disabled={active + 1 === total}>Next</Button>
+                    </Flex>
                 </Flex>
             </div>
         </div>
--- src/components/Slideshow.tsx
+++ src/components/Slideshow.tsx
@@ -5,15 +5,33 @@ import { Parallax, Navigation, Pagination, HashNavigation } from 'swiper/modules
 import slidesData from '../assets/data/slides.json';
 import { ISlide } from '../models/slide';
 import CustomSlide from './CustomSlide';
+import { Button, Flex, Modal } from 'antd';
+import TextArea from 'antd/es/input/TextArea';
+import { FacebookShareButton } from 'react-share';
+
+interface ModalState {
+  isVisible: boolean;
+  channel?: string;
+  data?: string;
+}
 
 const Slideshow: FC = () => {
   const swiperRef = useRef<SwiperClass | null>(null);
   const [slides, setSlides] = useState<ISlide[]>([]);
   const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [formData, setFormData] = useState<Record<number, any>>({});
+  const [modalState, setModalState] = useState<ModalState>({ isVisible: false });
 
   useEffect(() => {
     setSlides(slidesData);
   }, []);
+
+  const updateFormData = (slideIndex: number, data: any) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [slideIndex]: data,
+    }));
+  };
 
   const handleSlideChange = (swiper: SwiperClass) => {
     setActiveIndex(swiper.activeIndex);
@@ -24,12 +42,13 @@ const Slideshow: FC = () => {
     if (swiperRef.current) swiperRef.current?.slideTo(slideIndex);
   };
 
-  const handleNext = () => {
-    if (swiperRef.current) swiperRef.current.slideNext();
+  const openShareModal = () => {
+    const slideData = formData[activeIndex] || {};
+    setModalState({ isVisible: true, channel: slideData.channel, data: slideData.comment });
   };
 
-  const handleBack = () => {
-    if (swiperRef.current) swiperRef.current.slidePrev();
+  const closeShareModal = () => {
+    setModalState({ isVisible: false });
   };
 
   return (
@@ -39,6 +58,7 @@ const Slideshow: FC = () => {
         speed={600}
         autoHeight
         direction="vertical"
+        allowTouchMove={false}
         hashNavigation={{ watchState: true }}
         onSlideChange={handleSlideChange}
         modules={[Parallax, Pagination, Navigation, HashNavigation]}
@@ -46,17 +66,56 @@ const Slideshow: FC = () => {
       >
         {slides.map((slide, index) => (
           <SwiperSlide key={index} data-hash={slide.title} className="slide">
-            <CustomSlide 
+            <CustomSlide
               slide={slide}
               active={activeIndex}
               total={slides.length}
+              formData={formData}
+              updateForm={updateFormData}
+              shareForm={openShareModal}
               goto={goToSlide}
-              onPrev={handleBack}
-              onNext={handleNext} 
             />
           </SwiperSlide>
         ))}
       </Swiper>
+      <Modal
+        title="Confirm Share Post"
+        open={modalState.isVisible}
+        onCancel={closeShareModal}
+        footer={() => {
+          return (
+            <Flex align='center' justify='flex-end' gap={'4px'}>
+              <Button key="cancel" onClick={closeShareModal}>
+                Cancel
+              </Button>,
+              <FacebookShareButton
+                key="share"
+                url={`https://www.youtube.com/@${modalState.channel}`}
+                hashtag={modalState.data}
+                onClick={closeShareModal}
+              >
+                <Button key="submit" type="primary" >
+                  Share
+                </Button>
+              </FacebookShareButton>
+            </Flex>
+          )
+        }}
+      >
+        <div>
+          <p>Preview and edit your post below:</p>
+          <TextArea
+            rows={6}
+            value={modalState.data}
+            onChange={(e) =>
+              setModalState((prevState) => ({
+                ...prevState,
+                data: e.target.value,
+              }))
+            }
+          />
+        </div>
+      </Modal>
     </div>
   );
 };
